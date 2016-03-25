43683b432b97316b3561b1425fbbf044b2ca63d9
alan.zhang <alan.zhang@kakaocorp.com>
2016-03-25 11:24:35 +0800

    初始化

---
+++ ImagePicker/ImagePicker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImagePicker
+//
+//  Created by kakao on 16/3/25.
+//  Copyright © 2016年 shuai zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
